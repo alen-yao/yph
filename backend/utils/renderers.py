5468c9c23eb8e0939bcb6b6be7f5b5c1a20127ed
YAO Alen (BD/ISA-PRC1) <ylo7szh@bosch.com>
2026-08-15 14:20:12 +0800

    H5 update

--- backend/utils/renderers.py
+++ backend/utils/renderers.py
@@ -1,23 +1,38 @@
 from rest_framework.renderers import JSONRenderer
+from rest_framework.utils import json
 
 
 class UTF8JSONRenderer(JSONRenderer):
-    """自定义 JSON 渲染器，确保中文正确显示"""
+    """
+    自定义 JSON 渲染器，使中文不被转义为 Unicode
+    设置 ensure_ascii=False 使中文正常显示
+    """
     charset = 'utf-8'
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        """重写 render 方法，使用 ensure_ascii=False"""
+        """
+        重写 render 方法，强制使用 ensure_ascii=False
+        """
         if data is None:
             return b''
 
-        ret = self.encoder_class(
-            ensure_ascii=False,
-            allow_nan=not self.strict,
-            indent=self.get_indent(accepted_media_type, renderer_context),
-            separators=self.get_separators()
-        ).encode(data)
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context)
 
-        # 确保返回的是 bytes
-        if isinstance(ret, str):
-            return ret.encode(self.charset)
-        return ret
+        if indent is None:
+            separators = (',', ':')
+        else:
+            separators = (',', ': ')
+
+        ret = json.dumps(
+            data,
+            cls=self.encoder_class,
+            indent=indent,
+            ensure_ascii=False,  # 关键：不转义非 ASCII 字符
+            allow_nan=not self.strict,
+            separators=separators
+        )
+
+        # 返回 UTF-8 编码的字节
+        ret = ret.replace(' ', '\\u2028').replace(' ', '\\u2029')
+        return ret.encode(self.charset)
