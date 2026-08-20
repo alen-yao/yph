c9f4e7b7601a7109086c6956687ef6991eeee563
YAO Alen (BD/ISA-PRC1) <ylo7szh@bosch.com>
2026-08-20 18:51:30 +0800

    H5 update

--- frontend/pc/vite.config.js
+++ frontend/pc/vite.config.js
@@ -15,6 +15,10 @@ export default defineConfig({
       '/api': {
         target: 'http://backend:8000',
         changeOrigin: true
+      },
+      '/media': {
+        target: 'http://backend:8000',
+        changeOrigin: true
       }
     }
   }
